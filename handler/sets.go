baa65f251598e0a016a63e3fb2846488e3c756c1
Herbie <hjrand1005@gmail.com>
2022-02-06 14:49:32 -0500

    Initial commit for standard-library api implementation

--- handler/sets.go
+++ handler/sets.go
@@ -1,34 +1,86 @@
 package handler
 
 import (
+    //"fmt"
     "log"
     "net/http"
-    "strconv"
+    //"strconv"
 
     "github.com/hrand1005/training-notebook/data"
-	"github.com/gin-gonic/gin"
 )
 
-func CreateSet(c *gin.Context) {
-	var newSet data.Set
-
-	if err := c.BindJSON(&newSet); err != nil {
-		log.Printf("could not bind json to set: %v", err)
-		return
-	}
-
-    // assigns ID to newSet
-    data.AddSet(&newSet)
-	c.IndentedJSON(http.StatusCreated, newSet)
-	return
+type Set struct {
+    logger *log.Logger
 }
 
-func ReadSets(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, data.Sets())
+func NewSet(l *log.Logger) *Set {
+    return &Set{l}
+}
+
+func (s *Set) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case http.MethodGet:
+        s.ReadAll(rw, r)
+    case http.MethodPost:
+        s.Create(rw, r)
+    case http.MethodPut:
+        s.Update(rw, r)
+    /*case http.MethodDelete:
+        s.Delete(rw, r)*/
+    default:
+        http.Error(rw, "method not allowed", http.StatusMethodNotAllowed)
+    }
     return
 }
 
-func ReadSet(c *gin.Context) {
+func (s *Set) ReadAll(rw http.ResponseWriter, r *http.Request) {
+    if err := data.ToJSON(data.Sets(), rw); err != nil {
+        s.logger.Printf("could not serialize sets to json: %v", err)
+        rw.WriteHeader(http.StatusBadRequest)
+    }
+
+    return
+}
+
+func (s *Set) Create(rw http.ResponseWriter, r *http.Request) {
+	var newSet data.Set
+
+	if err := data.FromJSON(&newSet, r.Body); err != nil {
+		s.logger.Printf("could not bind json to set: %v", err)
+        rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+    // assigns ID to newSet
+    data.AddSet(&newSet)
+
+    rw.WriteHeader(http.StatusCreated)
+    data.ToJSON(newSet, rw)
+	return
+}
+
+func (s *Set) Update(rw http.ResponseWriter, r *http.Request) {
+	var newSet data.Set
+
+	if err := data.FromJSON(&newSet, r.Body); err != nil {
+		s.logger.Printf("could not bind json to set: %v", err)
+        rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+    if err := data.UpdateSet(&newSet); err != nil {
+        s.logger.Printf("could not update set: %v", err)
+        rw.WriteHeader(http.StatusNotFound)
+        return
+    }
+
+    data.ToJSON(newSet, rw)
+    return
+}
+
+
+// REQUIRE ID PARAM
+/*
+func (s *Set) Read(c *gin.Context) {
 	id, _ := strconv.Atoi(c.Param("id"))
 
     s, err := data.SetByID(id)
@@ -42,26 +94,7 @@ func ReadSet(c *gin.Context) {
     return
 }
 
-func UpdateSet(c *gin.Context) {
-	var newSet data.Set
-
-	if err := c.BindJSON(&newSet); err != nil {
-		log.Printf("could not bind json to set: %v", err)
-		return
-	}
-
-    // assigns newSet ID of id
-    if err := data.UpdateSet(&newSet); err != nil {
-        log.Printf("could not update set: %v", err)
-        c.IndentedJSON(http.StatusNotFound, gin.H{"message": "set not found"})
-        return
-    }
-
-    c.IndentedJSON(http.StatusOK, newSet)
-    return
-}
-
-func DeleteSet(c *gin.Context) {
+func (s *Set) Delete(c *gin.Context) {
     id, _ := strconv.Atoi(c.Param("id"))
     
     if err := data.DeleteSet(id); err != nil {
@@ -73,3 +106,5 @@ func DeleteSet(c *gin.Context) {
     c.IndentedJSON(http.StatusNoContent, gin.H{})
     return
 }
+*/
+
--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "context"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+    "time"
 	"github.com/hrand1005/training-notebook/handler"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/sets", handler.ReadSets)
-	router.GET("/sets/:id", handler.ReadSet)
-	router.POST("/sets", handler.CreateSet)
-	router.PUT("/sets", handler.UpdateSet)
-	router.DELETE("/sets/:id", handler.DeleteSet)
+    logger := log.New(os.Stdout, "training-api", log.LstdFlags)
+    setHandler := handler.NewSet(logger)
 
-	router.Run("localhost:8080")
+    serveMux := http.NewServeMux()
+    serveMux.Handle("/sets", setHandler)
+
+    server := &http.Server{
+        Addr: ":8080",
+        Handler: serveMux,
+        IdleTimeout: 120*time.Second,
+        ReadTimeout: 1*time.Second,
+        WriteTimeout: 1*time.Second,
+    }
+
+    go func() {
+        if err := server.ListenAndServe(); err != nil {
+            logger.Fatal(err)
+        }
+    }()
+
+    sigChan := make(chan os.Signal)
+    signal.Notify(sigChan, os.Interrupt)
+    signal.Notify(sigChan, os.Kill)
+
+    sig := <-sigChan
+    logger.Println("Recieved exit signal, executing graceful shutdown\nsignal: ", sig)
+
+    timeout, _ := context.WithTimeout(context.Background(), 30*time.Second)
+    server.Shutdown(timeout)
 }
