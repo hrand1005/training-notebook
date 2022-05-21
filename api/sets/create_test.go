bb53ff809193d960914e25e7a4d923e5b6531959
Herbie <hjrand1005@gmail.com>
2022-05-21 01:24:04 -0400

    ADD: create_test checks response body

--- api/sets/create_test.go
+++ api/sets/create_test.go
@@ -1,6 +1,7 @@
 package sets
 
 import (
+	"bytes"
 	"net/http/httptest"
 	"testing"
 
@@ -8,7 +9,6 @@ import (
 	"github.com/hrand1005/training-notebook/data"
 )
 
-// TODO: setup mock db
 func TestCreateSet(t *testing.T) {
 
 	// test cases:
@@ -17,47 +17,64 @@ func TestCreateSet(t *testing.T) {
 
 	testCases := []struct {
 		name     string
+		data     []*data.Set
 		wantCode int
 		set      data.Set
-		//		wantResp *bytes.Buffer
+		wantResp bytes.Buffer
 	}{
 		{
-			name:     "Set created returns 200",
+			name:     "Valid set created returns 200",
 			wantCode: 201,
 			set: data.Set{
 				Movement:  "Barbell Curl",
 				Volume:    12,
 				Intensity: 0.5,
 			},
-			//wantResp:
+			wantResp: *bytes.NewBufferString(` {
+					"id": 1,
+					"movement": "Barbell Curl",
+					"volume": 12,
+					"intensity": 0.5
+				} `),
 		},
-		// TODO: Bad request case
 	}
 
 	for _, v := range testCases {
-		// start with empty setData
-		testData := data.NewSetData(nil)
-
+		// configure test case with data and test context
+		testData := data.NewSetData(v.data)
 		ts, err := New(testData)
 		if err != nil {
 			t.Fail()
 		}
 
 		gin.SetMode(gin.TestMode)
-
 		w := httptest.NewRecorder()
-
 		c, _ := gin.CreateTestContext(w)
 
+		// set 'newSet' in the context
 		c.Set("newSet", v.set)
 
+		// execute create with the test context
 		ts.Create(c)
 
-		if w.Code != v.wantCode {
-			// TODO: print body
+		// check response code
+		if v.wantCode != w.Code {
 			t.Fatalf("Wanted code: %v\nGot code: %v\n", v.wantCode, w.Code)
 		}
-		// TODO: compare body
-		// TODO: ADD CLEANUP!!!
+
+		// check response body
+		if equal, _ := JSONBytesEqual(v.wantResp.Bytes(), w.Body.Bytes()); !equal {
+			t.Fatalf("Wanted body: %v\nGot body: %v\n", v.wantResp.String(), w.Body.String())
+		}
 	}
 }
+
+// TODO: Decide how to Test Set Validator Middleware
+// {
+// 	name:     "Invalid set returns 400",
+// 	wantCode: 400,
+// 	set:      data.Set{},
+// 	wantResp: *bytes.NewBufferString(` {
+// 		"message": "invalid request parameters"
+// 		} `),
+// },
