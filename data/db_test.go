195870b8655eaa86980aebcbee24f68ebbc96cd8
Herbie <hjrand1005@gmail.com>
2022-05-26 19:52:52 -0400

    ADD: TestSets to db with helpers

--- data/db_test.go
+++ data/db_test.go
@@ -34,7 +34,6 @@ func TestAddSet(t *testing.T) {
 
 	for _, v := range testCases {
 		sd := setupTestSetDB()
-		defer teardownTestSetDB()
 
 		id, err := sd.AddSet(v.set)
 		if err != nil {
@@ -48,7 +47,114 @@ func TestAddSet(t *testing.T) {
 		if !setExists {
 			t.Fatalf("Failed db check: %v", msg)
 		}
+
+		teardownTestSetDB(sd)
 	}
+}
+
+func TestSets(t *testing.T) {
+	testCases := []struct {
+		name     string
+		addSets  []*Set
+		wantSets []*Set
+	}{
+		{
+			name: "No added sets returns empty slice",
+		},
+		{
+			name: "ID value before insertion does not affect Sets result",
+			addSets: []*Set{
+				{
+					ID:        -1,
+					Movement:  "High jump",
+					Volume:    1,
+					Intensity: 90,
+				},
+			},
+			wantSets: []*Set{
+				{
+					Movement:  "High jump",
+					Volume:    1,
+					Intensity: 90,
+				},
+			},
+		},
+		{
+			name: "Multiple added sets each appear in returned Sets",
+			addSets: []*Set{
+				{
+					ID:        -1,
+					Movement:  "Yeet ball",
+					Volume:    10,
+					Intensity: 50,
+				},
+				{
+					ID:        -1,
+					Movement:  "Farmer's carry",
+					Volume:    30,
+					Intensity: 20,
+				},
+				{
+					ID:        -1,
+					Movement:  "Lateral raise",
+					Volume:    12,
+					Intensity: 60,
+				},
+			},
+			wantSets: []*Set{
+				{
+					Movement:  "Yeet ball",
+					Volume:    10,
+					Intensity: 50,
+				},
+				{
+					Movement:  "Farmer's carry",
+					Volume:    30,
+					Intensity: 20,
+				},
+				{
+					Movement:  "Lateral raise",
+					Volume:    12,
+					Intensity: 60,
+				},
+			},
+		},
+	}
+	for _, v := range testCases {
+		sd := setupTestSetDB()
+
+		// add 'addSets' to the database
+		for _, s := range v.addSets {
+			_, err := sd.AddSet(s)
+			if err != nil {
+				t.Fatalf("Encountered unexpected error: %v", err)
+			}
+		}
+
+		// check that each of 'wantSets' is returned
+		sets, err := sd.Sets()
+		if err != nil {
+			t.Fatalf("Encountered unexpected error: %v", err)
+		}
+		if len(sets) != len(v.wantSets) {
+			t.Fatalf("Wanted length %v but got %v\nWant: %+v\nGot: %+v", len(v.wantSets), len(sets), v.wantSets, sets)
+		}
+		for _, wantSet := range v.wantSets {
+			if !containsSet(sets, wantSet) {
+				t.Fatalf("Did not find expected set in sets response\nWanted: %+v\nFull Response: %+v", wantSet, sets)
+			}
+		}
+
+		// check that ids were not assigned invalid values
+		for _, s := range sets {
+			if s.ID <= 0 {
+				t.Fatalf("Invalid set id: %v", s.ID)
+			}
+		}
+
+		teardownTestSetDB(sd)
+	}
+
 }
 
 const testSetDB = "testSetDB.sqlite"
@@ -56,14 +162,26 @@ const testSetDB = "testSetDB.sqlite"
 func setupTestSetDB() *setDB {
 	sd, err := newSetDB(testSetDB)
 	if err != nil {
-		panic("failed to create testSetDB.sqlite")
+		msg := fmt.Sprintf("failed to setup test db: %v, err: %v", testSetDB, err)
+		panic(msg)
 	}
 
 	return sd
 }
 
-func teardownTestSetDB() {
+func teardownTestSetDB(sd *setDB) {
+	sd.handle.Close()
 	os.Remove(testSetDB)
+}
+
+// containsSet checks if the slice of sets contains the provided set.
+func containsSet(sets []*Set, s *Set) bool {
+	for _, v := range sets {
+		if s.Movement == v.Movement && s.Volume == v.Volume && s.Intensity == v.Intensity {
+			return true
+		}
+	}
+	return false
 }
 
 // checkSetInDB is a testing utility that checks whehter the provided set exists in the
