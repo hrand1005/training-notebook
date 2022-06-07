8ac4eaa599054e381b690c3a51da5a50fec861ba
Herbie <hjrand1005@gmail.com>
2022-06-06 23:19:39 -0400

    ADD: delete operation on users resource in api layer

---
+++ api/users/delete.go
@@ -0,0 +1,38 @@
+package users
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hrand1005/training-notebook/data"
+)
+
+// swagger:route DELETE /users/{id} users deleteuser
+// Delete a user.
+// responses:
+//  204: noContent
+//  404: errorResponse
+//  500: errorResponse
+
+// Delete is the handler for delete requests on the user resource. An id must be
+// specified.
+func (u *user) Delete(c *gin.Context) {
+	userID, err := userIDFromParams(c)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": ErrInvalidUserID})
+		return
+	}
+
+	if err := u.db.DeleteUser(userID); err != nil {
+		if err == data.ErrNotFound {
+			msg := fmt.Sprintf("no such user with id %v", userID)
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": msg})
+			return
+		}
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.IndentedJSON(http.StatusNoContent, gin.H{})
+}
---
+++ api/users/delete_test.go
@@ -0,0 +1,98 @@
+package users
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hrand1005/training-notebook/data"
+	"github.com/hrand1005/training-notebook/models"
+)
+
+// TestDeleteUser tests the API layer's Delete method for the Users resource.
+// The test suite mocks the UserDB interface to test edge cases and error conditions.
+func TestDeleteUser(t *testing.T) {
+	tests := []struct {
+		name     string
+		db       *data.MockUserDB
+		id       string
+		wantCode int
+		wantResp bytes.Buffer
+	}{
+		{
+			name: "User found with valid db call returns StatusNoContent",
+			db: &data.MockUserDB{
+				DeleteUserStub: func(id models.UserID) error {
+					return nil
+				},
+			},
+			id:       "1",
+			wantCode: http.StatusNoContent,
+		},
+		{
+			name: "User not found returns StatusNotFound",
+			db: &data.MockUserDB{
+				DeleteUserStub: func(id models.UserID) error {
+					return data.ErrNotFound
+				},
+			},
+			id:       "4",
+			wantCode: http.StatusNotFound,
+			wantResp: *bytes.NewBufferString(`{
+				"message": "no such user with id 4"
+			}`),
+		},
+		{
+			name: "Invalid db query returns InternalServerError",
+			db: &data.MockUserDB{
+				DeleteUserStub: func(id models.UserID) error {
+					return fmt.Errorf("Expected error")
+				},
+			},
+			id:       "4",
+			wantCode: http.StatusInternalServerError,
+			wantResp: *bytes.NewBufferString(`{
+				"message": "Expected error"
+			}`),
+		},
+		{
+			name:     "Invalid params returns StatusBadRequest",
+			id:       "-1",
+			wantCode: http.StatusBadRequest,
+			wantResp: *bytes.NewBufferString(fmt.Sprintf(`{
+				"message": %q
+			}`, ErrInvalidUserID)),
+		},
+	}
+
+	for _, v := range tests {
+		// configure test case with data and test context
+		ts, err := New(v.db)
+		if err != nil {
+			t.Fail()
+		}
+
+		gin.SetMode(gin.TestMode)
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		c.AddParam("id", v.id)
+
+		// execute Delete on test context
+		ts.Delete(c)
+
+		// check response code
+		if v.wantCode != w.Code {
+			t.Fatalf("Wanted code: %v\nGot code: %v\n", v.wantCode, w.Code)
+		}
+
+		if v.wantCode != http.StatusNoContent {
+			// check response body
+			if equal, _ := JSONBytesEqual(v.wantResp.Bytes(), w.Body.Bytes()); !equal {
+				t.Fatalf("Wanted body: %v\nGot body: %v\n", v.wantResp.String(), w.Body.String())
+			}
+		}
+	}
+}
