7f105715452a39dbbcbbbbeee4de030c2cb57acb
Herbie <hjrand1005@gmail.com>
2022-08-14 09:53:14 -0400

    ADD: proof of concept, decoding to bson.M and struct types

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -36,10 +37,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create mongo db client: %v", err)
 	}
+	defer client.Disconnect(ctx)
 
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		log.Fatalf("Failed to ping mongo db: %v", err)
 	}
+
 
 	mode := os.Getenv("MODE")
 	if mode == "test" {
@@ -51,14 +54,40 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/bson-map", func(c *fiber.Ctx) error {
 		var results []bson.M
-		cursor, err := client.Database(db).Collection("test").Find(ctx, bson.D{})
-		cursor.All(ctx, &results)
-
+		cur, err := client.Database(db).Collection("test").Find(ctx, bson.D{})
 		if err != nil {
-			return fmt.Errorf("get request on '/': %v", err)
+			return fmt.Errorf("get request on '/bson-map': %v", err)
 		}
+		if err = cur.All(ctx, &results); err != nil {
+			return fmt.Errorf("get request on '/bson-map': %v", err)
+		}
+		fmt.Printf("Results as bson mapping:\n%+v", results)
+
+		return c.JSON(results)
+	})
+
+	app.Get("/struct", func(c *fiber.Ctx) error {
+		type result struct {
+			ID primitive.ObjectID `bson:"_id"`
+			Key string `bson:"key,omitempty"`
+		}
+		var results []result
+		cur, err := client.Database(db).Collection("test").Find(ctx, bson.D{})
+		if err != nil {
+			return fmt.Errorf("get request on '/struct': %v", err)
+		}
+		defer cur.Close(ctx)
+		for cur.Next(ctx) {
+			r := result{}
+			if err := cur.Decode(&r); err != nil {
+				return fmt.Errorf("get request on '/struct': %v", err)
+			}
+			results = append(results, r)
+		}
+		fmt.Printf("Results in struct slice:\n%+v", results)
+
 		return c.JSON(results)
 	})
 
