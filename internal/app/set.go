f09a80bf8b94c57498c62ab2fc7cc65ca3f89b75
Herbie <hjrand1005@gmail.com>
2022-09-14 19:47:12 -0400

    ADD: set service and store domain logic

--- internal/app/set.go
+++ internal/app/set.go
@@ -1,11 +1,63 @@
 package app
 
+import (
+	"fmt"
+)
+
 type SetID string
+
+const InvalidSetID = "0"
 
 type Set struct {
 	ID        SetID
 	OwnerID   UserID
-	Movement  string
-	Intensity float64
-	Volume    int
+	Movement  string  `validate:"required,min=2,max=32"`
+	Intensity float64 `validate:"required,min=0,max=100"`
+	Volume    int     `validate:"required,min=1,max=100"`
+}
+
+type SetStore interface {
+	Insert(*Set) (SetID, error)
+	UpdateByID(SetID, *Set) error
+	FindByID(SetID) (*Set, error)
+	DeleteByID(SetID) error
+}
+
+type SetService interface {
+	Create(*Set) (SetID, error)
+	ReadByID(SetID) (*Set, error)
+	UpdateByID(SetID, *Set) error
+	DeleteByID(SetID) error
+}
+
+type setService struct {
+	store SetStore
+}
+
+func NewSetService(store SetStore) SetService {
+	return &setService{
+		store: store,
+	}
+}
+
+func (s *setService) Create(u *Set) (SetID, error) {
+	if err := ValidateEntity(u); err != nil {
+		return InvalidSetID, fmt.Errorf("SetService.Create.ValidateEntity: %w", err)
+	}
+	return s.store.Insert(u)
+}
+
+func (s *setService) ReadByID(id SetID) (*Set, error) {
+	return s.store.FindByID(id)
+}
+
+func (s *setService) UpdateByID(id SetID, u *Set) error {
+	if err := ValidateEntity(u); err != nil {
+		return fmt.Errorf("SetService.UpdateByID.ValidateEntity: %w", err)
+	}
+	return s.store.UpdateByID(id, u)
+}
+
+func (s *setService) DeleteByID(id SetID) error {
+	return s.store.DeleteByID(id)
 }
